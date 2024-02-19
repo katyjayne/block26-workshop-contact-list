880bfecdd142835c16eb61f6b627a784b01443eb
katyjayne <katyjayneblank@gmail.com>
2024-02-19 14:39:13 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ContactList} from "./components/ContactList";
+import './App.css'
+
+export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ContactList />
+    </>
+  )
+}
+
+export default App
